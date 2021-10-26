5ef1704ef86ee6fc50866f34e3f03465a5a1e7f2
Stephen Bailey <stkbailey@gmail.com>
2021-10-25 21:58:15 -0400

    Update state partitioning keys and add more fields to post_process

--- tap_slack/streams.py
+++ tap_slack/streams.py
@@ -56,10 +56,16 @@ class ChannelMembersStream(SlackStream):
     schema = schemas.channel_members
 
     ignore_parent_replication_keys = True
+    state_partitioning_keys = []
 
     def parse_response(self, response):
         user_list = extract_jsonpath(self.records_jsonpath, input=response.json())
         yield from ({"member_id": ii} for ii in user_list)
+
+    def post_process(self, row, context=None):
+        row = super().post_process(row, context=context)
+        row["channel_id"] = context.get("channel_id")
+        return row
 
 
 class MessagesStream(SlackStream):
@@ -135,10 +141,12 @@ class ThreadsStream(SlackStream):
     max_requests_per_minute = 50
     schema = schemas.threads
 
-    @property
-    def state_partitioning_keys(self):
-        "Remove thread_ts to prevent state logging for individual threads."
-        return ["channel_id"]
+    state_partitioning_keys = []
+
+    def post_process(self, row, context=None):
+        row = super().post_process(row, context=context)
+        row["channel_id"] = context.get("channel_id")
+        return row
 
 
 class UsersStream(SlackStream):
