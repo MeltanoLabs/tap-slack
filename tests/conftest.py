9756738d90bfb484dbb34e9b420975d3ae9321d7
Stephen Bailey <stkbailey@gmail.com>
2021-10-28 20:59:34 -0400

    Add expectation testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,22 +7,80 @@ from tap_slack.tap import TapSlack
 from tap_slack.testing import StreamTestUtility
 
 
-@pytest.fixture(scope="session")
-def sample_config():
-    yield {
-        "api_key": os.environ.get("TAP_SLACK_API_KEY"),
-        "start_date": "2021-09-25T00:00:00Z",
-    }
-
+SAMPLE_CONFIG = {
+    "api_key": os.environ.get("TAP_SLACK_API_KEY"),
+    "start_date": "2021-09-25T00:00:00Z",
+}
 
 @pytest.fixture(scope="session")
-def tap_slack(sample_config):
-    yield TapSlack(config=sample_config)
+def tap_slack():
+    yield TapSlack(config=SAMPLE_CONFIG)
 
 
 @pytest.fixture(scope="session")
-def stream_test_util(sample_config):
-    test_util = StreamTestUtility(TapSlack, sample_config)
+def stream_test_util():
+    test_util = StreamTestUtility(TapSlack, SAMPLE_CONFIG)
     test_util.run_sync()
 
     yield test_util
+
+
+def pytest_generate_tests(metafunc):
+    stream_names = ["channels", "users", "messages", "channel_members", "threads"]
+    if "stream_tests" in metafunc.fixturenames:
+        if "stream_name" in metafunc.fixturenames:
+            metafunc.parametrize("stream_name", stream_names)
+        else:
+            raise Exception("stream_name is required parameter for this test.")
+
+
+@pytest.fixture
+def stream_tests(stream_name):
+    tests = {
+        "channels": {
+            "stream_tests": [
+                "_test_stream_catalog_schema_matches_records",
+                "_test_stream_record_schema_matches_catalog",
+                "_test_stream_returns_at_least_one_record",
+                "_test_stream_primary_key"
+            ],
+            "attribute_tests": {
+                "id": [
+                    "_test_stream_attribute_is_unique",
+                    "_test_stream_attribute_is_not_null"
+                ]
+            }
+        },
+        "channel_members": {
+            "stream_tests": [
+                "_test_stream_catalog_schema_matches_records",
+                "_test_stream_record_schema_matches_catalog",
+                "_test_stream_returns_at_least_one_record",
+                "_test_stream_primary_key"
+            ],
+            "attribute_tests": {}
+        },
+        "messages": {
+            "stream_tests": [
+                "_test_stream_catalog_schema_matches_records",
+                "_test_stream_record_schema_matches_catalog",
+                "_test_stream_returns_at_least_one_record",
+                "_test_stream_primary_key"
+            ],
+            "attribute_tests": {}
+        },
+        "threads": {
+            "stream_tests": [],
+            "attribute_tests": {}
+        },
+        "users": {
+            "stream_tests": [
+                "_test_stream_primary_key"
+            ],
+            "attribute_tests": {}
+        },
+    }
+
+    yield tests[stream_name]
+
+
--- tests/test_core.py
+++ tests/test_core.py
@@ -12,44 +12,55 @@ def test_standard_tap_tests(sample_config):
     for test in tests:
         test()
 
-def test_channels_stream(stream_test_util):
-    stream_name = "channels"
-    stream_test_util._test_stream_catalog_schema_matches_records(stream_name)
-    stream_test_util._test_stream_record_schema_matches_catalog(stream_name)
-    stream_test_util._test_stream_returns_at_least_one_record(stream_name)
-    stream_test_util._test_stream_primary_key(stream_name)
+def test_standard_stream_tests(stream_test_util, stream_name, stream_tests):
+    for test_name in stream_tests["stream_tests"]:
+        test = getattr(stream_test_util, test_name)
+        test(stream_name)
+    
+def test_standard_stream_attribute_tests(stream_test_util, stream_name, stream_tests):
+    for k in stream_tests["attribute_tests"].keys():
+        for test_name in stream_tests["attribute_tests"][k]:
+            test = getattr(stream_test_util, test_name)
+            test(stream_name, k)
+
+# def test_channels_stream(stream_test_util):
+#     stream_name = "channels"
+#     stream_test_util._test_stream_catalog_schema_matches_records(stream_name)
+#     stream_test_util._test_stream_record_schema_matches_catalog(stream_name)
+#     stream_test_util._test_stream_returns_at_least_one_record(stream_name)
+#     stream_test_util._test_stream_primary_key(stream_name)
 
 
-def test_channel_members_stream(stream_test_util):
-    stream_name = "channel_members"
-    stream_test_util._test_stream_catalog_schema_matches_records(stream_name)
-    stream_test_util._test_stream_record_schema_matches_catalog(stream_name)
-    stream_test_util._test_stream_returns_at_least_one_record(stream_name)
-    stream_test_util._test_stream_primary_key(stream_name)
+# def test_channel_members_stream(stream_test_util):
+#     stream_name = "channel_members"
+#     stream_test_util._test_stream_catalog_schema_matches_records(stream_name)
+#     stream_test_util._test_stream_record_schema_matches_catalog(stream_name)
+#     stream_test_util._test_stream_returns_at_least_one_record(stream_name)
+#     stream_test_util._test_stream_primary_key(stream_name)
 
 
-def test_messages_stream(stream_test_util):
-    stream_name = "messages"
-    # stream_test_util._test_stream_catalog_schema_matches_records(stream_name)
-    stream_test_util._test_stream_record_schema_matches_catalog(stream_name)
-    stream_test_util._test_stream_returns_at_least_one_record(stream_name)
-    stream_test_util._test_stream_primary_key(stream_name)
+# def test_messages_stream(stream_test_util):
+#     stream_name = "messages"
+#     # stream_test_util._test_stream_catalog_schema_matches_records(stream_name)
+#     stream_test_util._test_stream_record_schema_matches_catalog(stream_name)
+#     stream_test_util._test_stream_returns_at_least_one_record(stream_name)
+#     stream_test_util._test_stream_primary_key(stream_name)
 
 
-def test_threads_stream(stream_test_util):
-    stream_name = "threads"
-    # stream_test_util._test_stream_catalog_schema_matches_records(stream_name)
-    stream_test_util._test_stream_record_schema_matches_catalog(stream_name)
-    stream_test_util._test_stream_returns_at_least_one_record(stream_name)
-    stream_test_util._test_stream_primary_key(stream_name)
+# def test_threads_stream(stream_test_util):
+#     stream_name = "threads"
+#     # stream_test_util._test_stream_catalog_schema_matches_records(stream_name)
+#     stream_test_util._test_stream_record_schema_matches_catalog(stream_name)
+#     stream_test_util._test_stream_returns_at_least_one_record(stream_name)
+#     stream_test_util._test_stream_primary_key(stream_name)
 
-    stream_test_util._test_stream_attribute_is_unique(stream_name, "ts")
-    # stream_test_util._test_stream_attribute_is_valid_timestamp(stream_name, "ts")
-    stream_test_util._test_stream_attribute_is_not_null(stream_name, "ts")
+#     stream_test_util._test_stream_attribute_is_unique(stream_name, "ts")
+#     # stream_test_util._test_stream_attribute_is_valid_timestamp(stream_name, "ts")
+#     stream_test_util._test_stream_attribute_is_not_null(stream_name, "ts")
 
-def test_users_stream(stream_test_util):
-    stream_name = "users"
-    stream_test_util._test_stream_catalog_schema_matches_records(stream_name)
-    stream_test_util._test_stream_record_schema_matches_catalog(stream_name)
-    stream_test_util._test_stream_returns_at_least_one_record(stream_name)
-    stream_test_util._test_stream_primary_key(stream_name)
+# def test_users_stream(stream_test_util):
+#     stream_name = "users"
+#     stream_test_util._test_stream_catalog_schema_matches_records(stream_name)
+#     stream_test_util._test_stream_record_schema_matches_catalog(stream_name)
+#     stream_test_util._test_stream_returns_at_least_one_record(stream_name)
+#     stream_test_util._test_stream_primary_key(stream_name)
