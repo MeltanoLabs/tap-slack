851cd42787b4f84080a09cf9470a9515dd2cb949
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-06-18 17:51:55 -0600

    feat: Modernize tap
    - Support Python 3.9+
    - Pin a minor version for the Singer SDK
    - Use built-in integration tests
    - Switch from Poetry to uv
    - Add dependabot
    - Run tests in CI

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,46 +1,27 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-import pytest
 import os
-from unittest.mock import patch
+import datetime
 
 from tap_slack.tap import TapSlack
-from tap_slack.testing import TapTestUtility
+
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
 
 SAMPLE_CONFIG = {
     "api_key": os.environ.get("TAP_SLACK_API_KEY"),
-    "start_date": "2021-09-25T00:00:00Z",
+    "start_date": (datetime.datetime.now() - datetime.timedelta(days=3)).isoformat(),
 }
 
-test_utility = TapTestUtility(TapSlack, SAMPLE_CONFIG, stream_record_limit=500)
-test_utility.tap.run_discovery()
-test_utility.run_sync()
 
-pytest_params = test_utility.generate_built_in_tests()
-
-
-@pytest.fixture(scope="session")
-def test_util():
-    yield test_utility
-
-
-@pytest.mark.parametrize("test_config", **pytest_params)
-def test_builtin_tap_tests(test_util, test_config):
-    test_name, params = test_config
-    test_func = test_util.available_tests[test_name]
-    test_func(**params)
-
-
-@patch("tap_slack.streams.ChannelsStream._join_channel")
-def test_auto_join_channel_false(patch_obj, test_util):
-    test_utility.run_sync()
-    patch_obj.assert_not_called()
-
-@patch("tap_slack.streams.ChannelsStream._join_channel")
-def test_auto_join_channel(patch_obj, test_util):
-    config = SAMPLE_CONFIG.copy()
-    config["auto_join_channels"] = True
-    test_utility = TapTestUtility(TapSlack, config, stream_record_limit=500)
-    test_utility.run_sync()
-    patch_obj.assert_called()
+TestTapSlack = get_tap_test_class(
+    TapSlack,
+    config=SAMPLE_CONFIG,
+    suite_config=SuiteConfig(
+        ignore_no_records_for_streams=[
+            "channels",
+            "messages",
+            "threads",
+        ],
+    ),
+)
